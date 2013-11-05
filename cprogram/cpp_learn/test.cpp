6b06269c1386440b90889daf78b5fe91cc022099
Jensen <wangji0604@live.cn>
2013-11-05 14:19:53 +0800

    just for test

--- cprogram/cpp_learn/test.cpp
+++ cprogram/cpp_learn/test.cpp
@@ -5,15 +5,21 @@
 #include <fstream>
 using namespace std;
 
-void func()
+void func( int ic)
 {
-	static int i = 0;
-	i =0;
-	cout << ++i << endl;
+	const int& i = ic;
+	ic++;
+	cout << "i=" << i << endl
+		<< "ic=" << ic << endl;
 }
 int main()
 {
-	for(int i=0; i<10; i++)
-		func();
+//	for(int i=0; i<10; i++)
+//		func();
+//	const char* ch = "hello";
+//	cout << ch << endl;
+	func(5);
+	int i = 5;
+	func(i);
 	return 0;
 }
